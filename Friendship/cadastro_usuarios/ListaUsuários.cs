7f6f579ccaad83a84f3eed247acf40fdb5666f48
ddb <ddb@ICI-90842>
2021-05-12 19:56:18 -0300

    TEntando

---
+++ Friendship/cadastro_usuarios/ListaUsuários.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+namespace CSharp_ListaObjetos.Services
+{
+    class ListaUsuario
+    {
+        public static List<Usuario> ListaUsuarios()
+        {
+            var lstaUsuario = new List<Usuario>()
+            {
+                new Livro { LivroId=1,Titulo="ASP, ADO e Banco de dados na web",Autor="Macoratti", ISBN="8585943963"},
+                new Livro { LivroId=2,Titulo="ASP - Aprenda Rápido",Autor="Macoratti", ISBN="8585845214"}
+            };
+            return lstaUsuario;
+        }
+    }
+}
